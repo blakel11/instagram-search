2942218df792aef870345607b3428e07592b3261
Blake Lieberman <blakelieberman@gmail.com>
2015-02-26 09:36:14 -0800

    staging

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
-angular.module('searchApp', ['ngMessages'])
-	.config()
-	.controller('SearchCtrl', ['$scope, $q, $timeout, $http', function($scope, $q, $timeout, $http){
-		$scope.search = 
-	}]);
+angular.module('searchApp', [])
+	.controller('SearchCtrl', function($scope, $q, $timeout, $http){
+		$scope.searchInstagram = function(keyword){
+			console.log('click');
+			$scope.keyword = keyword;
+
+			var url = "https://api.instagram.com/v1/tags/" + $scope.keyword + "/media/recent";
+			var request = {
+			  callback: 'JSON_CALLBACK',
+			  client_id: '5232366fc1da49dfa7a343b1c9907d46',
+			  count: 10
+			};
+
+			$http({
+				method : 'GET',
+				url : url ,
+				params : request
+			})
+			.success(function(data){
+				console.log(data);
+				$scope.results = data;
+			})
+			.error(function(data){
+				console.log('Error');
+			});
+		};
+	});
